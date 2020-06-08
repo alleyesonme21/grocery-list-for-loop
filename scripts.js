2ef6c78f6b8d1103a53dd1a3a34a3a074526f3e3
heyimmatt <mrmcfarland@gmail.com>
2020-06-08 14:00:56 -0700

    Add the jQuery and js logic for taking the list input string, uppercasing everything, pushing the items to the array, and sort the array, and then dislay the modified grocery list
    Co-authored-by: Megan Hepner <megan.hepner@gmail.com>

--- scripts.js
+++ scripts.js
@@ -4,7 +4,12 @@ $(document).ready(function() {
     const groceryItemsString = $('#groceries').val();
 
     if (groceryItemsString) {
-      
+      $('#grocery-list').toggle();
+      $('#updated-grocery-list').toggle();
+      const listItems = groceryItemsString.toUpperCase().split(', ').sort();
+      listItems.forEach(item => {
+        $('#modified-grocery-list').append(`<li>${item}</li>`)
+      });
     } else alert('Please enter your grocery list items')
   })
 })
